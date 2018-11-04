b5784c55245c2d62c131c51c3bdd1ff3fe2ac1c5
Sam Calvert <sam.clvrt@gmail.com>
2018-11-04 15:01:33 -0800

    Hlint source files

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,7 @@
 
 module Lib where
 
-import Control.Concurrent (MVar, newMVar, modifyMVar_, modifyMVar, readMVar)
+import Control.Concurrent (MVar, newMVar, modifyMVar_, readMVar)
 import Control.Exception (finally)
 import Control.Monad (forever, forM_)
 
@@ -11,6 +11,7 @@ import Data.UUID (UUID, toText)
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Text.Encoding (decodeUtf8)
 
 import System.Random (randomIO)
 
@@ -24,53 +25,58 @@ type ServerState = M.Map Room [Client]
 mainApplication :: IO ()
 mainApplication = do
     state <- newMVar newServerState
-    putStrLn "Starting server on localhost port 9160..."
     WS.runServer "127.0.0.1" 9160 $ application state
+    putStrLn "Started server on http://localhost:9160"
 
 newServerState :: ServerState
 newServerState = M.empty
 
 addClientToRoom :: Client -> Room -> ServerState -> ServerState
-addClientToRoom client room state = M.alter (pure . insertOrAdd client) room state
+addClientToRoom client = M.alter (pure . insertOrAdd client)
     where
         insertOrAdd :: a -> Maybe [a] -> [a]
         insertOrAdd a (Just as) = a:as
         insertOrAdd a Nothing = [a]
 
 removeClientFromRoom :: Client -> Room -> ServerState -> ServerState
-removeClientFromRoom (uuid, _) room state = M.adjust (filter ((/= uuid) . fst)) room state
+removeClientFromRoom (uuid, _) = M.adjust (filter $ (/= uuid) . fst)
 
 sendError :: WS.Connection -> T.Text -> IO ()
 sendError conn msg = WS.sendTextData conn $ "Error: " <> msg
 
 application :: MVar ServerState -> WS.ServerApp
 application state pending = do
+    let requestedRoom = findRoom pending
+
+    T.putStrLn $ "Requested Room: " <> requestedRoom
+
     conn <- WS.acceptRequest pending
     WS.forkPingThread conn 30
 
-    uuid <- (randomIO :: IO UUID)
+    uuid <- randomIO :: IO UUID
     let client = (uuid, conn)
 
-    T.putStrLn $ "Connected to " <> (toText uuid)
+    T.putStrLn $ "Connected to " <> toText uuid
 
-    requestedRoom <- WS.receiveData conn
     case requestedRoom of
-        _ | any ($ requestedRoom) [T.any isSpace, (/= 10) . T.length] ->
+        _ | isNotValidRoom requestedRoom ->
               sendError conn "Invalid room id"
           | otherwise -> flip finally (disconnect client requestedRoom) $ do
               modifyMVar_ state $ \s -> do
                   let s' = addClientToRoom client requestedRoom s
                   WS.sendTextData conn $ toText uuid
-                  return s'
+                  pure s'
               listenForeverToClientInRoom client requestedRoom state
 
     where
         disconnect client room = modifyMVar_ state (pure . removeClientFromRoom client room)
+        findRoom = T.drop 1 . decodeUtf8 . WS.requestPath . WS.pendingRequest
+        isNotValidRoom room = not $ any ($ room) [T.any isSpace, (/= 10) . T.length]
 
 broadcastToRoom :: Room -> UUID -> T.Text -> ServerState -> IO ()
 broadcastToRoom room uuid message state = do
     let conns = filter ((/= uuid) . fst) $ M.findWithDefault [] room state
-    T.putStrLn (room <> " (" <> (T.pack . show $ 1 + length conns) <> " members): " <> message)
+    T.putStrLn $ room <> " (" <> (T.pack . show $ 1 + length conns) <> " members): " <> message
     forM_ conns $ \(_, conn) -> WS.sendTextData conn message
 
 listenForeverToClientInRoom :: Client -> Room -> MVar ServerState -> IO ()
